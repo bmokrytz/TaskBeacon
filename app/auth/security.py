e0a40f364a312d14716dea4577e5ea9f3f6da27e
bmokrytz <bmokrytz@uoguelph.ca>
2026-02-10 14:19:40 -0500

    feat: register endpoint - add register endpoint - add docstrings to existing methods

--- app/auth/security.py
+++ app/auth/security.py
@@ -1,6 +1,13 @@
 import bcrypt
 
 def hash_password(password: str) ->str:
+    """
+    Create a hash for plaintext password.
+    - Convert plaintext password to bytes (utf-8)
+    - Generate salt
+    - Generate hash
+    - Return decoded hash (utf-8)
+    """
     # bcrypt expects bytes
     password_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
@@ -8,6 +15,11 @@ def hash_password(password: str) ->str:
     return hashed.decode("utf-8")
 
 def verify_password(password: str, password_hash: str) -> bool:
+    """
+    Verify password matches hash.
+    - Convert both password and hash to bytes (utf-8)
+    - Check password against hash and return bool
+    """
     password_bytes = password.encode("utf-8")
     password_hash_bytes = password_hash.encode("utf-8")
     return bcrypt.checkpw(password_bytes, password_hash_bytes)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routers.health import router as health_endpoint_router
 from app.routers.tasks import router as tasks_endpoint_router
+from app.routers.auth import router as auth_endpoint_router
 import logging
 
 logging.basicConfig(
@@ -11,3 +12,4 @@ app = FastAPI(title="TaskBeacon", version="0.1.0")
 
 app.include_router(health_endpoint_router)
 app.include_router(tasks_endpoint_router)
+app.include_router(auth_endpoint_router)
