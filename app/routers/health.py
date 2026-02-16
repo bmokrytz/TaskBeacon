1d9339e9674c5d9f796e7973cd8d1415aeb283f0
bmokrytz <bmokrytz@uoguelph.ca>
2026-02-16 14:10:24 -0500

    feat: db config + connection - add dependencies to requirements.txt - add db config in app/core/config.py - add db session dependency in app/db/session.py - add db ping endpoint to app/routers/health.py

--- app/routers/health.py
+++ app/routers/health.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from app.db.session import get_db
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,4 +14,13 @@ def health_endpoint():
     - Returns 200 OK if service is running
     """
     logger.info("Sending Health Check")
+    return {"status": "ok"}
+
+@router.get("/db-ping")
+def db_ping(db: Session = Depends(get_db)):
+    """
+    Database service health check.
+    - Returns 200 OK if service is running
+    """
+    db.execute(text("SELECT 1"))
     return {"status": "ok"}
