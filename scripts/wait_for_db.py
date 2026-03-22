99155fd4451a403b8889632c4ef59969eb04d60d
bmokrytz <bmokrytz@uoguelph.ca>
2026-03-22 14:06:13 -0400

    fix: apprunner supabase connection - modify script to print any exception not just OperationalError

--- scripts/wait_for_db.py
+++ scripts/wait_for_db.py
@@ -3,12 +3,14 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 import os
+import traceback
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 MAX_RETRIES = 20
 DELAY = 2  # seconds
 
+print("Using DATABASE_URL:", DATABASE_URL.replace(os.getenv("DB_PASSWORD", ""), "****"))
 print("Waiting for database...")
 
 for i in range(MAX_RETRIES):
@@ -17,8 +19,9 @@ for i in range(MAX_RETRIES):
         with engine.connect():
             print("Database is ready.")
             sys.exit(0)
-    except OperationalError as e:
-        print(f"Database not ready yet... updated string - retry {i+1}/{MAX_RETRIES}: {e}")
+    except Exception as e:
+        print(f"Database not ready yet... retry {i+1}/{MAX_RETRIES}: {repr(e)}")
+        traceback.print_exc()
         time.sleep(DELAY)
 
 print("Database never became ready. Exiting.")
